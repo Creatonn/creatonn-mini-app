b749c9802850cf4729bb6b77a32fcfeae3a1c6da
EMEA2\A200179082 <yogita.sawant@t-systems.com>
2024-09-17 21:43:09 +0530

    testing wallet connect trial - 1

--- src/App.js
+++ src/App.js
@@ -5,20 +5,38 @@ import Header from './components/Header';
 import Profile from './components/Profile';
 import Leadership from './components/Leadership';
 import UserProfile from './components/UserProfile'
+import { SDKProvider, useLaunchParams } from '@telegram-apps/sdk-react';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import { useEffect, useMemo } from 'react';
 
 function App() {
+  const debug = useLaunchParams().startParam === 'debug';
+  const manifestUrl = useMemo(() => {
+    return new URL('tonconnect-manifest.json', window.location.href).toString();
+  }, []);
+
+  // Enable debug mode to see all the methods sent and events received.
+  useEffect(() => {
+    if (debug) {
+      import('eruda').then((lib) => lib.default.init());
+    }
+  }, [debug]);
+
   return (
-    <Router>
-      <div className="App bg-[#add8e6]">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/leaders" element={<Leadership />} />
-          <Route path="/profile/:userId" element={<UserProfile />} />
-        </Routes>
-      </div>
-    </Router>
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <SDKProvider acceptCustomStyles debug={debug}>
+        <Router>
+          <div className="App bg-[#add8e6]">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/leaders" element={<Leadership />} />
+              <Route path="/profile/:userId" element={<UserProfile />} />
+            </Routes>
+          </div>
+        </Router>
+      </SDKProvider></TonConnectUIProvider>
   );
 }
 
